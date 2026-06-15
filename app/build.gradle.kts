2d760c0c9796e18b7f7ee6ce8725c6b5c8538d00
kibetkorir395 <kibetkorir395@gmail.com>
2026-06-15 14:46:21 +0300

    updated commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.codesui.footballfixtures"
+        applicationId = "com.codesui.football"
         minSdk = 24
         targetSdk = 34
         versionCode = 3
